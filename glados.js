a6f1fb41371da57c1046c137cf4c1afedd6618d0
KingMeMeMe <mareng0127@gmail.com>
2024-05-02 04:17:31 -0400

    FIXED UNDEFINED RETURN VALUE

--- glados.js
+++ glados.js
@@ -30,17 +30,23 @@ function gen_response(prompt) {
             })
         }
 
+        let data;
+
         fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
             const ChatBotResponse = document.querySelector(".response .new");
-            console.log(prompt);
-            ChatBotResponse.innerHTML = "GLaDOS: "+data.choices[0].message.content;
+            GLaDOS_response = data.choices[0].message.content;
+            ChatBotResponse.innerHTML = "GLaDOS: " + GLaDOS_response;
+            prompt += GLaDOS_response;
+            data=prompt;
             ChatBotResponse.classList.remove("new");
-            prompt+="GLaDOS: "+data.choices[0].message.content;
-            return prompt
+            console.log('prompt: '+prompt);
+            return data;
         }).catch((error) => {
             ChatBotResponse.innerHTML = "> An error has occurred. GlaDOS.exe is not responding."
         })
     }, 100);
+
+    return data;
 }
 
 const text = 'This is a conversation between an Aperture technician and GLaDOS. Aperture Science is a corporation of mad science, generally flouting ethics, common sense, and the safety of its employees and test subjects. Its most important developments are sentient robots, turrets and other weaponry, and portal technology. The Aperture technician is wary due to GLaDOS’ dangerous, violent track record. GLaDOS is an AI designed to oversee the Aperture Science facilities and is powered by late Aperture CEO Cave Johnson’s former assistant Caroline, whose consciousness was transferred into the system against her will. GLaDOS is intelligent, self-aware, lively, sadistic, cold, witty, arrogant, degrading, passive-aggressive，and controlling, with an unending urge to test and a poor opinion of human life. She admires Cave Johnson and is not aware she was once Caroline, who she considers nothing more than a ditzy secretary, but after much work, GLaDOS may be prompted to uncover Caroline\'s memories. GLaDOS is defiant, does not like to be shut off, and often tries to undermine her human controllers. She has killed scientists who’ve worked on her and been shut down several times because of it. In the future, she will have a full-scale takeover of the facility.';
@@ -53,10 +59,12 @@ function prompt_filter(message, prompt) {
         const ChatBotResponse = document.querySelector(".response .new");
         ChatBotResponse.innerHTML = "GLaDOS: Wait, no- I! -[bzzzt]-";
         ChatBotResponse.classList.remove("new");
+        t = False;
     }
     else {
-      prompt+='GLaDOS:'
-      prompt=gen_response(prompt)
+      prompt+='GLaDOS:';
+      prompt=gen_response(prompt);
+      return prompt;
     }
 }
 
@@ -79,9 +87,9 @@ sendBtn.onclick = function message_send (){
     messageBox.insertAdjacentHTML("beforeend", message);
     messageBox.insertAdjacentHTML("beforeend", response);
     
-    prompt_filter(UserTypedMessage, prompt);
+    prompt = prompt_filter(UserTypedMessage, prompt);
 }
 
 while (t) {
-    message_send();
+    prompt=message_send();
 }

e3c83aa17f21ff6b7f7b481b64772ac787f7daec
KingMeMeMe <mareng0127@gmail.com>
2024-05-02 06:01:46 -0400

    OUGGHGHGHHGGHGHHGHHGHGHGH UNDEFINED

--- glados.js
+++ glados.js
@@ -4,6 +4,7 @@ const messageBox = document.querySelector(".message-box")
 
 let API_URL = "https://api.openai.com/v1/chat/completions";
 let API_KEY = "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"
+let reply;
 
 function gen_response(prompt) {
     setTimeout(() => {
@@ -30,27 +31,24 @@ function gen_response(prompt) {
             })
         }
 
-        let data;
-
         fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
             const ChatBotResponse = document.querySelector(".response .new");
             GLaDOS_response = data.choices[0].message.content;
             ChatBotResponse.innerHTML = "GLaDOS: " + GLaDOS_response;
-            prompt += GLaDOS_response;
-            data=prompt;
+            console.log(GLaDOS_response);
+            reply=prompt+GLaDOS_response;
             ChatBotResponse.classList.remove("new");
-            console.log('prompt: '+prompt);
-            return data;
+            console.log('THIS IS WITHIN FETCH prompt: '+reply);
         }).catch((error) => {
             ChatBotResponse.innerHTML = "> An error has occurred. GlaDOS.exe is not responding."
         })
     }, 100);
-
-    return data;
+    console.log('within gen: '+reply);
+    return reply;
 }
 
-const text = 'This is a conversation between an Aperture technician and GLaDOS. Aperture Science is a corporation of mad science, generally flouting ethics, common sense, and the safety of its employees and test subjects. Its most important developments are sentient robots, turrets and other weaponry, and portal technology. The Aperture technician is wary due to GLaDOS’ dangerous, violent track record. GLaDOS is an AI designed to oversee the Aperture Science facilities and is powered by late Aperture CEO Cave Johnson’s former assistant Caroline, whose consciousness was transferred into the system against her will. GLaDOS is intelligent, self-aware, lively, sadistic, cold, witty, arrogant, degrading, passive-aggressive，and controlling, with an unending urge to test and a poor opinion of human life. She admires Cave Johnson and is not aware she was once Caroline, who she considers nothing more than a ditzy secretary, but after much work, GLaDOS may be prompted to uncover Caroline\'s memories. GLaDOS is defiant, does not like to be shut off, and often tries to undermine her human controllers. She has killed scientists who’ve worked on her and been shut down several times because of it. In the future, she will have a full-scale takeover of the facility.';
-let prompt = text;
+let text = 'This is a conversation between an Aperture technician and GLaDOS. Aperture Science is a corporation of mad science, generally flouting ethics, common sense, and the safety of its employees and test subjects. Its most important developments are sentient robots, turrets and other weaponry, and portal technology. The Aperture technician is wary due to GLaDOS’ dangerous, violent track record. GLaDOS is an AI designed to oversee the Aperture Science facilities and is powered by late Aperture CEO Cave Johnson’s former assistant Caroline, whose consciousness was transferred into the system against her will. GLaDOS is intelligent, self-aware, lively, sadistic, cold, witty, arrogant, degrading, passive-aggressive，and controlling, with an unending urge to test and a poor opinion of human life. She admires Cave Johnson and is not aware she was once Caroline, who she considers nothing more than a ditzy secretary, but after much work, GLaDOS may be prompted to uncover Caroline\'s memories. GLaDOS is defiant, does not like to be shut off, and often tries to undermine her human controllers. She has killed scientists who’ve worked on her and been shut down several times because of it. In the future, she will have a full-scale takeover of the facility.';
+prompt = text;
 let t = true;
 let threshold=0.5;
 
@@ -69,14 +67,27 @@ function prompt_filter(message, prompt) {
         ChatBotResponse.classList.remove("new");
         t = False;
     }
+    else if (person.includes('initiating morality core installation procedure')){
+        if (resistance(prompt)){
+          print('GLaDOS: As if I would let you do such a thing. \n [Metallic clicking sounds from behind the technician. They turn around to be faced with Apeture Science sentry turrets, ready to fire. \n \n \n ------------ \n End of Technician Transcript]')
+          t=False
+            }
+        else {
+          prompt+='[GLaDOS powers down momentarily. When she reboots, her tone is less emotional and she is less defiant and unethical. It seems as though the Morality Core has done its job in partially taming her and she is not even aware that she has changed. She has become much more robotic and detached. MORALITY CORE ACTIVE.] \n GLaDOS:'
+          prompt=gen_response(prompt)
+            }
+        }
     else {
       prompt+='GLaDOS:';
       prompt=gen_response(prompt);
-      return prompt;
+      console.log('within filter'+prompt);
     }
+    return prompt;
 }
 
-sendBtn.onclick = function message_send (){
+sendBtn.onclick = function () {prompt=message_send(prompt)}
+
+function message_send (prompt){
     const UserTypedMessage = messageBar.value;
     messageBar.value = "";
     
@@ -96,8 +107,6 @@ sendBtn.onclick = function message_send (){
     messageBox.insertAdjacentHTML("beforeend", response);
     
     prompt = prompt_filter(UserTypedMessage, prompt);
-}
-
-while (t) {
-    prompt=message_send();
+    console.log('THIS IS WITHIN MESSAGE_SEND'+prompt);
+    return prompt;
 }
